671100149e8c585677a189d55c24c92d66757bb1
Aikon72 <aikon@bk.ru>
2025-02-12 18:57:24 +0500

    Начало задания 5

---
+++ src/HomeworkFive.java
@@ -0,0 +1,15 @@
+public class HomeworkFive {
+    public static void main(String[] args) {
+        //Задача №1
+        //Дан массив:
+        //int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        //необходимо вывести все нечетные числа из массива.
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        for (int i = 0; i <= array.length; i++){
+            if ( array[i] % 2 != 0){
+                System.out.printf("Элемент " + i + ": " + array[i]);
+            }
+        }
+    }
+}
