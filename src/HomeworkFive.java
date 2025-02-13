47f4e709891462cf268fa37562c39cc96114a403
Aikon72 <aikon@bk.ru>
2025-02-13 21:59:14 +0500

    Задание 5 сделано

--- src/HomeworkFive.java
+++ src/HomeworkFive.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class HomeworkFive {
     public static void main(String[] args) {
         /* Задача №1
@@ -11,5 +13,24 @@ public class HomeworkFive {
                 System.out.println("Элемент " + i + ": " + array[i]);
             }
         }
+        System.out.println("\nЗадача 2");
+        /* Задача №2
+        Дан массив:
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        необходимо вывести все значения массива больше 5.*/
+        for ( int i = 0; i < array.length; i++){
+            if ( array[i] > 5){
+                System.out.println("Элемент " + i + ": " + array[i]);
+            }
+        }
+        /*Задача №3
+        Дан массив:
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        необходимо увеличить все значения массива на 15.*/
+        System.out.println("\nЗадача 3");
+        for ( int i = 0; i < array.length; i++){
+            array[i] += 15;
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
